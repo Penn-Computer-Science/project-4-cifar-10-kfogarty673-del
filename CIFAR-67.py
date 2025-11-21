1645fdd3b8384a4964129684a9d0f21fd1642f37
kfogarty673-del <kfogarty673@phm.k12.in.us>
2025-11-21 08:39:16 -0500

    Update CIFAR-67.py

--- CIFAR-67.py
+++ CIFAR-67.py
@@ -3,35 +3,46 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 from keras.utils import to_categorical
 
 print(tf.__version__)
 keras = tf.keras
-from keras.datasets import cifar10
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+from keras.datasets import cifar100
+(x_train, y_train), (x_test, y_test) = cifar100.load_data()
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
+y_train = to_categorical(y_train, 100)
+y_test = to_categorical(y_test, 100)
 
 input_shape = 32, 32, 3
 
-batch_size =128
-num_classes = 10
+batch_size = 64
+num_classes = 100
 
 model = tf.keras.models.Sequential(
     [
-        tf.keras.layers.Conv2D(32, (5, 5), padding='same', activation='relu', input_shape=input_shape),
-        tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=input_shape),
+        tf.keras.layers.RandomRotation(0.1),
+        tf.keras.layers.RandomZoom(0.1),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.MaxPool2D(),
-        tf.keras.layers.Dropout(0.15),
-        tf.keras.layers.Conv2D(64, (5, 5), padding='same', activation='relu', input_shape=input_shape),
-        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=input_shape),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=input_shape),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=input_shape),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Dropout(0.1),
         tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(num_classes, activation='softmax'),
     ]
 )
 
-model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['acc'])
+model.compile(optimizer='adamW',loss='categorical_crossentropy', metrics=['acc'])
 
 history = model.fit(x_train, y_train,epochs=10,validation_split=(0.1))
